a16e0e1e2438a76e463c1c9d780fac520bb0f6a9
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-09-22 16:03:23 +0200

    Update logo

--- inst/hexsticker.R
+++ inst/hexsticker.R
@@ -2,32 +2,32 @@
 
 hexSticker::sticker(
   
-  subplot  = here::here("inst", "rlogo.png"),
-  package  = "rmendeley",
-  filename = here::here("man", "figures", "hexsticker.png"),
-  dpi      = 600,
+  subplot   = here::here("inst", "rlogo.png"),
+  package   = "rbibtools",
+  filename  = here::here("man", "figures", "hexsticker.png"),
+  dpi       = 600,
   
-  p_size    = 32.0,        # Title
+  p_size    = 34.0,        # Title
   u_size    =  8.0,        # URL
   p_family  = "Aller_Rg",
   
-  p_color   = "#3265B1",   # Title
-  h_fill    = "#ffffff",   # Background
-  h_color   = "#3265B1",   # Border
-  u_color   = "#3265B1",   # URL
+  p_color   = "#F9D349",   # Title
+  h_fill    = "#0D0823",   # Background
+  h_color   = "#3E3E3E",   # Border
+  u_color   = "#FEFDFF",   # URL
   
   p_x       = 1.00,        # Title
   p_y       = 0.55,        # Title
   s_x       = 1.00,        # Subplot
   s_y       = 1.20,        # Subplot
   
-  s_width   = 0.65,        # Subplot
-  s_height  = 0.65,        # Subplot
+  s_width   = 0.60,        # Subplot
+  s_height  = 0.60,        # Subplot
   
   spotlight = TRUE,
   l_alpha   = 0.10,
   l_width   = 4,
   l_height  = 4,
   
-  url      = "https://frbcesab.github.io/rmendeley"
+  url       = "https://github.com/frbcesab/"
 )
